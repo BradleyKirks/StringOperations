cd8b3f7e89233367f5cd8cb4f0438d76040c6ac9
Bradley Kirks <spinnyorb23@aim.com>
2023-10-10 12:16:16 -0400

    StringExamples

---
+++ index.js
@@ -0,0 +1,7 @@
+let firstName = 'todd'
+let age = 25 
+let str = 'hello JavaScript Students'
+let str2 = 'hello' //`hello ${firstName} you are ${age} today!`
+console.log(`hello ${firstName}`)
+
+console.log(str.length) //26
